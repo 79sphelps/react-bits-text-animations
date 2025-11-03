96eece38dae8e42e7c9b83a087ff6e57e463c7ac
Steve Phelps <79sphelps@gmail.com>
2025-11-03 08:35:59 -0800

    Adding ShinyText, Shuffle, and TextPressure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import SplitText from "./SplitText";
 import BlurText from "./BlurText";
-import CircularText from './CircularText';
-import TextType from './TextType';
+import CircularText from "./CircularText";
+import TextType from "./TextType";
+import Shuffle from "./Shuffle";
+import ShinyText from './ShinyText';
+import TextPressure from './TextPressure';
 
 function App() {
-const handleAnimationComplete = () => {
-  console.log('All letters have animated!');
-};
+  const handleAnimationComplete = () => {
+    console.log("All letters have animated!");
+  };
 
   return (
-    <div className="App">
-    {/* <div> */}
+    // <div className="background-gray-900 min-h-screen flex flex-col justify-center items-center space-y-12 p-4">
+    <div className="bg-blue-500 min-h-screen">
+
       <CircularText
-        text="REACT*BITS*COMPONENTS*"
+        text="CIRCULAR*TEXT*COMPONENT*"
         onHover="speedUp"
         spinDuration={20}
-        className="custom-class"
+        className="text-5xl font-semibold text-center text-blue-900"
       />
       <SplitText
-        text="Hello, GSAP!"
+        text="SplitText Component!"
         className="text-5xl font-semibold text-center"
         delay={100}
         duration={0.6}
@@ -33,37 +36,70 @@ const handleAnimationComplete = () => {
         textAlign="center"
         onLetterAnimationComplete={handleAnimationComplete}
       />
-      <BlurText
-        text="Isn't this so cool?!"
-        delay={150}
-        animateBy="words"
-        direction="top"
-        onAnimationComplete={handleAnimationComplete}
-        className="text-5xl mb-8 text-center"
-      />
-      <TextType 
-        text={["Text typing effect", "for your websites", "Happy coding!"]}
-        typingSpeed={75}
-        pauseDuration={1500}
-        showCursor={true}
-        cursorCharacter="|"
 
-        className="text-5xl font-medium text-center mt-8"
-      />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <div className="justify-center items-center flex flex-col">
+        <BlurText
+          text="This is the BlurText component!"
+          delay={150}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="text-5xl mb-8 text-center"
+        />
+      </div>
+      <div>
+        <TextType
+          text={["TextType component effect", "for your websites", "Awesome!"]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          showCursor={true}
+          cursorCharacter="|"
+          className="text-5xl font-medium text-center mt-8"
+        />
+      </div>
+      <div>
+        <Shuffle
+          text="This is the Shuffle component effect!"
+          shuffleDirection="right"
+          duration={0.35}
+          animationMode="evenodd"
+          shuffleTimes={1}
+          ease="power3.out"
+          stagger={0.03}
+          threshold={0.1}
+          triggerOnce={true}
+          triggerOnHover={true}
+          respectReducedMotion={true}
+          className="text-5xl font-semibold text-center mt-8"
+        />
+      </div>
+
+      <div>
+        <ShinyText 
+          text="This is the ShinyText component!" 
+          disabled={false} 
+          speed={3} 
+          className='text-5xl font-semibold text-center' 
+        />
+      </div>
+
+      {/* <div style={{position: 'relative', height: '300px'}}> */}
+      <div className="justify-center items-center flex flex-col mt-8 mb-8">
+        <TextPressure
+          text="Hello!"
+          flex={true}
+          alpha={false}
+          stroke={false}
+          width={true}
+          weight={true}
+          italic={true}
+          textColor="#ffffff"
+          strokeColor="#ff0000"
+          minFontSize={36}
+          // minFontSize={24}
+        />
+      </div>
+
     </div>
   );
 }
